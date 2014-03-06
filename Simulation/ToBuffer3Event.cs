bb85d3031afbbb20f3a12680c085bab25e5cb327
Willem Pino <wjapino@gmail.com>
2014-03-06 14:35:45 +0100

    check whether correct file online 2

---
+++ Simulation/ToBuffer3Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simulation
+{
+    class ToBuffer3Event : Event
+    {
+        public ToBuffer3Event()
+        {
+            // method to find time at which this event happens
+            // and add to EventList.eventList
+        }
+    }
+}
